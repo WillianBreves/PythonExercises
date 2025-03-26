e3b4bff41acb81520fc353539bf90eb04f178e53
WillianBreves <willianbreves.dev@gmail.com>
2025-03-26 10:48:47 -0300

    ex023.py

---
+++ ex023.py
@@ -0,0 +1,12 @@
+#Make a program that reads a number from 0 to 9999 and displays each of the digits separately on the screen.
+
+num = int(input('Informe um número:'))
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
+print('Analisando seu numero: {}'.format(num))
+print('Unidade: {}'.format(u))
+print('Dezena: {}'.format(d))
+print('Centena: {}'.format(c))
+print('Milhares: {}'.format(m))
